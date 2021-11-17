aa6c1cb085122785bb0fbb271dc103d8c5a187bd
Raimundo Amorim <rjunior_amorim@hotmail.com>
2021-11-17 17:43:22 -0300

    update space content services

--- src/components/Services/Right.tsx
+++ src/components/Services/Right.tsx
@@ -4,7 +4,7 @@ export default function ServiceLeft(props) {
   
   return (
     <>
-      <div className="flex flex-col md:flex-row transform transition-all duration-200 ease-in-out justify-between items-center container mx-auto text-gray-700 px-6 pb-6">
+      <div className="flex flex-col md:flex-row transform transition-all duration-200 ease-in-out justify-between items-center container mx-auto text-gray-700 px-6 py-12">
         <div className="flex flex-col justify-center flex-1">
           <h3 className="text-2xl">{props.title}</h3>
           <span className="border-b-2 border-primary-500 w-6/12 mb-4"></span>
